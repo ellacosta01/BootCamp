221b5995ea4d4d7041bf54fcb2c32bab21d768dc
ella.acostayuxi <ella.acostayuxi@U-3207F3QY340Y5.corp.amazonworkspaces.com>
2026-01-20 20:15:43 -0500

    Aplicacion de polimorfismo y herencia

---
+++ Mision1/Proyecto1/src/com/empresa/gestion/model/Persona.java
@@ -0,0 +1,27 @@
+package com.empresa.gestion.model;
+public class Persona {
+    //🔐 Encapsulación
+    private String nombre;
+    private int edad;
+    //🧱 Constructor
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+//Método
+    public void presentarse(){
+        System.out.println("Hola, soy"+nombre+" y tengo "+edad+" años");
+    }
+}
---
+++ Mision1/Proyecto1/src/com/empresa/gestion/service/NominaService.java
@@ -0,0 +1,11 @@
+
+package com.empresa.gestion.service;
+
+import com.empresa.gestion.model.Empleado;
+
+public class NominaService {
+    public double calcularSalarioAnual(Empleado empleado) {
+        return empleado.getSalario() * 12;
+        
+    }
+}
