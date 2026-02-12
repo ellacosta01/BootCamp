0304c818118a854b39efae3135fdcb2eeb3280bf
ellacosta01 <ella0102.ea@gmail.com>
2026-02-11 22:07:37 -0500

    Se agrego el metodo crear y  actualizar usuario y se configuro el archivo de conf application.yml

--- Mision2/PBpg/energia/src/main/java/com/talentotech/energia/controller/UserController.java
+++ Mision2/PBpg/energia/src/main/java/com/talentotech/energia/controller/UserController.java
@@ -3,6 +3,9 @@ import com.talentotech.energia.model.User;
 import com.talentotech.energia.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 @RestController
 @RequestMapping("/api/users")
 
@@ -20,5 +23,24 @@ public User create (@RequestBody User user) {
 public List<User> finDALL() {
     return userRepository.findAll();
 }
+// READ BY ID
+@GetMapping("/{id}")
+public User findById(@PathVariable Long id) {
+    return userRepository.findById(id).orElseThrow(() 
+    -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
 
 }
+//UPDATE
+@PutMapping("/{id}")
+public User update(@PathVariable Long id, @RequestBody User userDetails) {
+    User user = userRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+    user.setUsername(userDetails.getUsername());
+    user.setEmail(userDetails.getEmail());
+
+    return userRepository.save(user);
+}
+
+}
+
